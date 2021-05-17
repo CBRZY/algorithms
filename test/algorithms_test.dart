237980e2ac23832ce3f6b11fa8bd6f6a6e6f69f3
DigitalCapitan <captain@cbrzy.com>
2021-05-17 16:58:51 +1000

    Cleanup. All executions happen in tests

--- test/algorithms_test.dart
+++ test/algorithms_test.dart
@@ -3,31 +3,80 @@ import 'package:algorithms/quicksort.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Binary Search', () {
-    group('Iterative', () {
-      test('Item exists in list', () {
-        expect(search<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1),
-            {'guesses': 3, 'index': 0});
+  group('Binary Search -', () {
+    List<int> numbers = [];
+    for (int i = 1; i <= 1000000; i++) {
+      numbers.add(i);
+    }
+    group('Integers -', () {
+      group('Iterative -', () {
+        test('Item exists in list', () {
+          expect(
+            search<int>(numbers, 95632),
+            {'guesses': 20, 'index': 95631},
+          );
+        });
+        test('Item doesn\'t exist in list', () {
+          expect(
+            search<int>(numbers, -2),
+            {'guesses': 19, 'index': -1},
+          );
+        });
       });
-      test('Item doesn\'t exist in list', () {
-        expect(search<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1),
-            {'guesses': 3, 'index': -1});
+      group('Recursive -', () {
+        test('Item exists in list', () {
+          expect(
+            recursiveSearch<int>(numbers, 1000000),
+            {'guesses': 20, 'index': 999999},
+          );
+        });
+        test('Item doesn\'t exist in list', () {
+          expect(
+            recursiveSearch<int>(numbers, 10000000),
+            {'guesses': 20, 'index': -1},
+          );
+        });
       });
     });
-    group('Recursive', () {
-      test('Item exists in list', () {
-        expect(recursiveSearch<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2),
-            {'guesses': 2, 'index': 1});
+
+    group('Strings -', () {
+      group('Iterative -', () {
+        test('Item exists in list', () {
+          expect(
+            search<String>(
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 'b'),
+            {'guesses': 2, 'index': 1},
+          );
+        });
+        test('Item doesn\'t exist in list', () {
+          expect(
+            search<String>(
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 'z'),
+            {'guesses': 4, 'index': -1},
+          );
+        });
       });
-      test('Item doesn\'t exist in list', () {
-        expect(recursiveSearch<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -2),
-            {'guesses': 3, 'index': -1});
+      group('Recursive -', () {
+        test('Item exists in list', () {
+          expect(
+            recursiveSearch<String>(
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 'c'),
+            {'guesses': 3, 'index': 2},
+          );
+        });
+        test('Item doesn\'t exist in list', () {
+          expect(
+            recursiveSearch<String>(
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 'z'),
+            {'guesses': 4, 'index': -1},
+          );
+        });
       });
     });
   });
 
-  group('Quicksort', () {
-    group('Integers', () {
+  group('Quicksort -', () {
+    group('Integers -', () {
       test('Sort unsorted positive integers', () {
         expect(
           quicksort<int>([2, 3, 9, 1, 5, 8, 4, 7]),
@@ -42,7 +91,7 @@ void main() {
       });
     });
 
-    group('Strings', () {
+    group('Strings -', () {
       test('Sort unsorted string', () {
         expect(
           quicksort<String>(['b', 'a', 'n', 'z', 'p', 'c']),
